bcc1f4e5c8be92a47af8003e35487900078a0806
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-10-20 18:57:48 -0600

    :wrench: Remove unnecessary suppression.

--- java/src/main/java/com/github/ususdw/Main.java
+++ java/src/main/java/com/github/ususdw/Main.java
@@ -13,7 +13,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //noinspection InfiniteLoopStatement
         while (true) {
             System.out.print("?> ");
             String input = scanner.nextLine();
